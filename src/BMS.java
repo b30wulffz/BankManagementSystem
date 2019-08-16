8cd63b0ab07f9d4a6cfe1a7979087a329c2e7a25
b30wulffz <shlokpandey123@gmail.com>
2019-08-16 23:09:16 +0530

    Completed Bank Management System V:1.0.0

--- src/BMS.java
+++ src/BMS.java
@@ -9,7 +9,6 @@ import static java.util.Objects.isNull;
 
 class Bank {
     public static String name = "ICICI";
-//    public static List<Long> accNumDB =  new ArrayList<>();
 
     public static Map<Long, Customer> customerDB = new HashMap<>();
 
@@ -21,16 +20,42 @@ class Bank {
         customerDB.put(accNum, cust);
         System.out.println("Your unique account number : "+accNum);
         System.out.println("Customer Details Added To Bank Successfully!");
-//        accNumDB.add(accNum);
         cust.setAccNum(accNum);
         return accNum;
     }
 
+    public void removeCustomer(){
+        System.out.println("Enter the account number of the customer: ");
+        Scanner inp = new Scanner(System.in);
+        long accNum = inp.nextLong();
+        if(isNull(customerDB.get(accNum))){
+            System.out.println("Customer Not Found!");
+        }
+        else {
+            System.out.println("Are you sure: ");
+            boolean confirm = inp.nextBoolean();
+            if(confirm){
+                customerDB.remove(accNum);
+                System.out.println("Account: "+accNum+" successfully deleted!");
+            }
+            else{
+                System.out.println("Deletion Aborted!");
+            }
+        }
+    }
+
+    public void printDetails(){
+        System.out.println("The users and account details are as follows: ");
+        customerDB.entrySet().forEach(entry->{
+            System.out.println(entry.getKey() + " : " + entry.getValue().getName());
+        });
+
+    }
 
 }
 
 class BankAccount extends Bank{
-    private float balance;
+//    private float balance;
     public long currActive;
     public static Map<Long, Double> balanceDB = new HashMap<>();
 
@@ -114,6 +139,154 @@ class BankAccount extends Bank{
         }
     }
 
+    public void fundsTransfer(){
+        if(currActive!=0) {
+            System.out.print("Enter the account number of the recipient: ");
+            Scanner input = new Scanner(System.in);
+            long recipientAccNum = input.nextLong();
+            if(recipientAccNum == currActive) {
+                System.out.println("Recipient should not be same as sender!");
+                return;
+            }
+            if(isNull(customerDB.get(recipientAccNum))){
+                System.out.println("Recipient does not exist. Please try again later.");
+            }
+            else {
+                System.out.println("Enter the amount to be transferred: ");
+                double amount = input.nextDouble();
+                double balance = balanceDB.get(currActive);
+                if((balance-amount) <= 500.0) {
+                    System.out.println("Sorry! Transaction could be completed due to insufficient funds.");
+                }
+                else {
+                    if(isNull(balanceDB.get(recipientAccNum))){
+                        balanceDB.put(recipientAccNum, 0.0);
+                    }
+                    double netBalance = amount+balanceDB.get(recipientAccNum);
+                    balanceDB.put(recipientAccNum, netBalance);
+                    balanceDB.put(currActive, (balance-amount));
+                    System.out.println("Transaction is successfully completed!");
+                }
+            }
+        }
+        else {
+            System.out.println("User is not logged in!");
+        }
+    }
+}
+
+class ATM extends BankAccount{
+    private static Map<Long, Integer> atmPinDB = new HashMap<>();
+    public static long currActiveATM = 0;
+    public void loginATM(){
+        if(currActiveATM == 0) {
+            Scanner val = new Scanner(System.in);
+            long adhNum, accNum;
+            int atmPin;
+            System.out.println("Enter your Unique Account Number:");
+            accNum = val.nextLong();
+
+            if (isNull(customerDB.get(accNum))) {
+                System.out.println("Sorry! Account number does not exists.");
+            } else {
+                if(isNull(atmPinDB.get(accNum))){
+                    System.out.println("First time login!");
+
+                    System.out.println("Enter your Aadhaar Number:");
+                    adhNum = val.nextLong();
+                    if(customerDB.get(accNum).getAdhNum() == adhNum) {
+                        atmPin = ((int)(Math.random()*10000)+1000)%10000;
+                        atmPinDB.put(accNum, atmPin);
+                        System.out.println("Generated ATM pin: "+atmPin);
+                        if(isNull(balanceDB.get(accNum))) {
+                            balanceDB.put(accNum, 0.0);
+                        }
+                    }
+                    else {
+                        System.out.println("Login Details are Incorrect!");
+                    }
+                }
+
+                System.out.println("Please enter your ATM pin to continue: ");
+                atmPin = val.nextInt();
+                if(atmPinDB.get(accNum) == atmPin){
+                    currActiveATM = accNum;
+                    System.out.println("Login Successful!");
+                }
+                else {
+                    System.out.println("Login Details are Incorrect!");
+                }
+
+            }
+        }
+        else {
+            System.out.println("User is currently logged in. First logout to login again!");
+        }
+    }
+
+    public void depositATM(double amount){
+        if(currActiveATM!=0) {
+            if (amount > 0.0) {
+                balanceDB.put(currActiveATM, balanceDB.get(currActiveATM) + amount);
+                System.out.println("Transaction through ATM is Successful.");
+            } else {
+                System.out.println("Sorry! Amount cannot be null or negative.");
+            }
+        }
+        else {
+            System.out.println("User is not logged in!");
+        }
+    }
+
+    public void withdrawATM(double amount){
+        if(currActiveATM!=0) {
+            double balance = balanceDB.get(currActiveATM);
+            if (amount <= 0.0) {
+                System.out.println("Sorry! Amount cannot be null or negative.");
+            } else if ((balance - amount) <= 500.0) {
+                System.out.println("Sorry! The balance after deduction will be less than Rs 500.");
+                System.out.println("Transaction cannot be carried out.");
+            } else {
+                balance = balance - amount;
+                balanceDB.put(currActiveATM, balance);
+                System.out.println("Transaction successful!");
+                System.out.println("Please collect the cash from tray!");
+            }
+        }
+        else {
+            System.out.println("User is not logged in!");
+        }
+    }
+
+    public void checkBalanceATM(){
+        if(currActiveATM!=0) {
+            System.out.println("Your balance is : " + balanceDB.get(currActiveATM));
+        }
+        else {
+            System.out.println("User is not logged in!");
+        }
+    }
+
+    public void logoutATM(){
+        if(currActiveATM!= 0) {
+            currActiveATM = 0;
+            System.out.println("Successfully Logged Out!");
+            System.out.println("Thank you for using "+name+" bank ATM.");
+            System.out.println("Do visit us again!");
+        }
+        else {
+            System.out.println("First Login to Logout!");
+        }
+    }
+    public String getUserName() {
+        if(currActiveATM!= 0) {
+            return customerDB.get(currActiveATM).getName();
+        }
+        else {
+            return "<Error> User Not Logged In";
+        }
+    }
+
 }
 
 class Customer {
@@ -158,33 +331,70 @@ class Customer {
 
 public class BMS {
     public static void main(String[] args) {
-        System.out.println("Welcome to Bank Management System: ");
-        System.out.println("Press 1: to add a customer");
-        System.out.println("Press 2: to Login a customer's information");
-        System.out.println("Press 3: to remove a customer");
 
         Bank icici = new Bank();
         BankAccount customerAcc = new BankAccount();
+        ATM customerATM = new ATM();
+        boolean outerLoop = true;
+        while(outerLoop) {
+            System.out.println("\n------------------------------------------------------\n");
+            System.out.println("Welcome to Bank Management System: ");
+            System.out.println("Press 1: For Bank Control Panel");
+            System.out.println("Press 2: For Customer Bank Account Login");
+            System.out.println("Press 3: For ATM Login");
+            System.out.println("Press 4: To Exit");
 
-        while(true) {
             Scanner inp = new Scanner(System.in);
             System.out.print("> ");
             int opt = inp.nextInt();
             switch (opt) {
                 case 1:
                 {
-                    Customer newCust = new Customer();
-                    System.out.println("Proceed to add data to bank?");
-                    boolean proceed;
-                    proceed = inp.nextBoolean();
-                    if(proceed) {
-                        icici.addCustomerGenerateAccNum(newCust);
+                    boolean start = true;
+                    while (start) {
+                        System.out.println("\n------------------------------------------------------\n");
+                        System.out.println("Welcome to " + icici.name + " Bank.");
+                        System.out.println("Press 1: To add a customer");
+                        System.out.println("Press 2: To remove a customer");
+                        System.out.println("Press 3: To print list of customers");
+                        System.out.println("Press 4: To exit");
+                        System.out.print("-> ");
+                        int userOpt = inp.nextInt();
+                        switch (userOpt) {
+                            case 1: {
+                                Customer newCust = new Customer();
+                                System.out.println("Proceed to add data to bank?");
+                                boolean proceed;
+                                proceed = inp.nextBoolean();
+                                if (proceed) {
+                                    icici.addCustomerGenerateAccNum(newCust);
+                                }
+                            }
+                            break;
+                            case 2: {
+                                icici.removeCustomer();
+                            }
+                            break;
+                            case 3:{
+                                icici.printDetails();
+                            }
+                            break;
+                            case 4: {
+                                System.out.println("Exiting " + icici.name + " Bank Control Panel");
+                                start = false;
+                            }
+                            default: {
+                                System.out.println("Sorry! Wrong Input Detected. Please Try Again.");
+                            }
+                        }
                     }
                 }
                 break;
                 case 2: //login : customer
                 {
                     Scanner val = new Scanner(System.in);
+                    System.out.println("\n------------------------------------------------------\n");
+                    System.out.println("Welcome to "+new Bank().name+" bank account.");
                     long adhNum, accNum;
                     System.out.println("Enter your Aadhaar Number:");
                     adhNum = val.nextLong();
@@ -194,11 +404,13 @@ public class BMS {
                     if(customerAcc.currActive != 0) {
                         boolean start = true;
                         while(start) {
+                            System.out.println("------------------------------------------------------");
                             System.out.println("Hello "+ customerAcc.getUserName());
                             System.out.println("Press 1: To deposit amount");
                             System.out.println("Press 2: To withdraw amount");
                             System.out.println("Press 3: To check balance");
-                            System.out.println("Press 4: To logout");
+                            System.out.println("Press 4: To transfer funds");
+                            System.out.println("Press 5: To logout");
                             System.out.print("-> ");
                             int userOpt = inp.nextInt();
                             switch (userOpt) {
@@ -227,6 +439,11 @@ public class BMS {
                                 break;
                                 case 4:
                                 {
+                                    customerAcc.fundsTransfer();
+                                }
+                                break;
+                                case 5:
+                                {
                                     customerAcc.logout();
                                     start = false;
                                 }
@@ -240,7 +457,74 @@ public class BMS {
                     }
                 }
                 break;
-                case 3: //l
+                case 3:
+                {
+                    System.out.println("\n------------------------------------------------------\n");
+                    System.out.println("Welcome to "+new Bank().name+" bank ATM.");
+                    customerATM.loginATM();
+
+                    if(customerATM.currActiveATM != 0) {
+                        boolean start = true;
+                        while(start) {
+                            System.out.println("------------------------------------------------------");
+                            System.out.println("Hello "+ customerATM.getUserName());
+                            System.out.println("Welcome to "+new Bank().name+" bank ATM.");
+                            System.out.println("Press 1: To check balance");
+                            System.out.println("Press 2: To deposit amount");
+                            System.out.println("Press 3: To withdraw amount");
+                            System.out.println("Press 4: To logout");
+                            System.out.print("-> ");
+                            int userOpt = inp.nextInt();
+                            switch (userOpt) {
+                                case 1:
+                                {
+                                    customerATM.checkBalanceATM();
+                                }
+                                break;
+                                case 2:
+                                {
+                                    double amount;
+                                    System.out.println("Enter the amount to deposit");
+                                    System.out.print("--> ");
+                                    amount = inp.nextDouble();
+                                    customerATM.depositATM(amount);
+                                }
+                                break;
+                                case 3:
+                                {
+                                    double amount;
+                                    System.out.println("Enter the amount to withdraw");
+                                    System.out.print("--> ");
+                                    amount = inp.nextDouble();
+                                    customerATM.withdrawATM(amount);
+                                }
+                                break;
+                                case 4:
+                                {
+                                    customerATM.logoutATM();
+                                    start = false;
+                                }
+                                break;
+                                default:
+                                {
+                                    System.out.println("Sorry! Wrong Input Detected. Please Try Again.");
+                                }
+                            }
+                        }
+                    }
+                }
+                break;
+                case 4:
+                {
+                    outerLoop = false;
+                    System.out.println("Exiting!");
+                    System.out.println("\n------------------------------------------------------\n");
+                }
+                break;
+                default:
+                {
+                    System.out.println("Sorry! Wrong Input Detected. Please Try Again.");
+                }
             }
         }
     }
